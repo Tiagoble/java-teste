cacfa0c11bea3bb267b7b912406b051088bf8245
Tiagoble <ti.abreu.da.rocha@gmail.com>
2025-05-12 15:37:08 -0300

    Trabalhando com matrizes em App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,27 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello!");
+        
+        int[][] matriz = new int[3][3];
+
+        //primeira linha
+        matriz[0][0] = 1;
+        matriz[0][1] = 2;
+        matriz[0][2] = 3;
+
+        //segunda linha
+        matriz[1][0] = 4;
+        matriz[1][1] = 5;
+        matriz[1][2] = 6;
+
+        //terceira linha
+        matriz[2][0] = 7;
+        matriz[2][1] = 8;
+        matriz[2][2] = 9;
+
+        int[][] mat = {{1, 2, 3},
+                       {4, 5, 6},
+                       {7, 8, 9}};
+
+        System.out.println(mat[0][0]+" "+mat[0][1]+" "+mat[0][2]);
     }
 }
